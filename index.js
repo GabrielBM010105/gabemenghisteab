69dbf3b7aef37633d9f47c37033249f08efc0543
GabrielBM010105 <gabrielbmenghisteab05@gmail.com>
2025-07-23 02:20:42 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const box = document.getElementById('box');
+let x = 300;
+let y = 100;
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowRight') x += 10;
+    if (e.key === 'ArrowLeft') x -= 10;
+    if (e.key === 'ArrowUp') y -= 10;
+    if (e.key === 'ArrowDown') y += 10;
+
+    box.style.left = x + 'px';
+    box.style.top = y + 'px';
+});
